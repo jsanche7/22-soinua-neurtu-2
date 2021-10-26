dcc4e470886d1d01ca6e5a084f56342b97c5d238
jsanche7 <80990132+jsanche7@users.noreply.github.com>
2021-10-26 10:57:38 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,43 @@
 basic.forever(function () {
     if (input.soundLevel() > 50) {
-        basic.showNumber(5)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
     } else if (input.soundLevel() > 40) {
-        basic.showNumber(4)
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
     } else if (input.soundLevel() > 30) {
-        basic.showNumber(3)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
     } else if (input.soundLevel() > 20) {
-        basic.showNumber(2)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            `)
     } else {
-        basic.showNumber(1)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
     }
 })
